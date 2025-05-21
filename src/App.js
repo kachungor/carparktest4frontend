9c530116fecdf12bd70a98102181839c0bbb0896
kachungor <159103528+kachungor@users.noreply.github.com>
2025-05-21 15:01:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Spot1 from './Spot1';
-import Spot2 from './Spot2';
-import Spot3 from './Spot3';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import SpotPage from './SpotPage';
 import './App.css';
+
+// 車位配置 - 簡單修改這個數組即可增加/減少車位
+const PARKING_SPOTS = [7, 8, 9, 10, 11];
 
 function App() {
   return (
@@ -18,16 +19,24 @@ function App() {
               <div className="qr-code-info">
                 <p>測試連結:</p>
                 <ul>
-                  <li><a href="/spot1">1號車位</a></li>
-                  <li><a href="/spot2">2號車位</a></li>
-                  <li><a href="/spot3">3號車位</a></li>
+                  {/* 動態生成車位連結列表 */}
+                  {PARKING_SPOTS.map(spotId => (
+                    <li key={spotId}>
+                      <a href={`/spot/${spotId}`}>{spotId}號車位</a>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
           } />
-          <Route path="/spot1" element={<Spot1 />} />
-          <Route path="/spot2" element={<Spot2 />} />
-          <Route path="/spot3" element={<Spot3 />} />
+          
+          {/* 使用統一的路由模式 */}
+          <Route path="/spot/:id" element={<SpotPage />} />
+          
+          {/* 處理舊的URL路徑，重定向到新路徑 */}
+          <Route path="/spot1" element={<Navigate replace to="/spot/1" />} />
+          <Route path="/spot2" element={<Navigate replace to="/spot/2" />} />
+          <Route path="/spot3" element={<Navigate replace to="/spot/3" />} />
         </Routes>
       </div>
     </Router>
